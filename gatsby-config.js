7477bc67f4174c03c90fa61b5d14e82d28782143
Vishnu Dileesh <dev@vishnudileesh.com>
2020-09-28 20:13:19 +0530

    Add - fevicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,17 @@
 module.exports = {
+  siteMetadata: {
+    description: "Portfolio website of Vishnu Dileesh",
+    locale: "en",
+    title: "Vishnu Dileesh - Portfolio"
+  },
   plugins: [
     {
       resolve: "@wkocjan/gatsby-theme-intro",
       options: {
+        showThemeLogo: false,
         theme: "classic",
-          
       },
-          
     },
-      
   ],
   
 }
